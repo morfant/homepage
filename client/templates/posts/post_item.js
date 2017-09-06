c104cdf0c77e762a189d7961debd3b18ec4f8f8a
giy <giy.hands@gmail.com>
2017-09-07 00:25:42 +0900

    CV style

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,7 +1,19 @@
 Template.postItem.helpers({
+
+  ownPost: function () {
+    return this.userId === Meteor.userId();
+  },
+
+  desc: function () {
+    // Find desc of this post
+    return "멀티미티어 음악공연";
+
+  },
+
   domain: function () {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
   }
+
 });
---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,42 @@
+Template.postSubmit.created = function () {
+    Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+    errorMessage: function (field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
+Template.postSubmit.events({
+    'submit form': function (e) {
+        e.preventDefault();
+
+        var post = {
+            // url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val(),
+            role: $(e.target).find('[name=role]').val(),
+            date: $(e.target).find('[name=date]').val(),
+            venue: $(e.target).find('[name=venue]').val(),
+            text:  $(e.target).find('[name=text]').val(),
+        };
+
+        var errors = validatePost(post);
+        if (errors.title) return Session.set('postSubmitErrors', errors);
+
+        Meteor.call('postInsert', post, function (error, result) { // display the error to the user and abort 
+            if (error) return throwError(error.reason);
+
+            if (result.postExists) throwError('This title has already been posted');
+
+            Router.go('postPage', {
+                _id: result._id
+            });
+
+        });
+
+    }
+});
