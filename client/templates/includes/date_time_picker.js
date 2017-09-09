b959055e5cef6a3e74c5e3c23f1b53db5e743843
giy <giy.hands@gmail.com>
2017-09-09 20:05:26 +0900

    Add moment.js. Store date as 'Date' to server.

--- client/templates/includes/date_time_picker.js
+++ client/templates/includes/date_time_picker.js
@@ -1,22 +1,32 @@
+Template.dateTimePicker.onCreated(function() {
+    Session.set("date_begin", "");
+    Session.set("date_end", "");
+
+});
+
 Template.dateTimePicker.onRendered(function() {
     this.$('.datetimepicker_begin').datetimepicker();
     this.$('.datetimepicker_end').datetimepicker({
-        // useCurrent: false;
+        useCurrent: false
     });
-
 });
 
 
 
 Template.dateTimePicker.events({
 
-    '.datetimepicker_begin': function (e) {
+    'dp.change .datetimepicker_begin': function (e) {
         e.preventDefault();
-        $('.datetimepicker_end').data("DateTimePicker").minDate(e.date);
+        var date = moment(e.date).toDate();
+        Session.set("date_begin", date);
+        // console.log("date_begin: " + date);
+
     },
-    '.datetimepicker_end': function (e) {
+    'dp.change .datetimepicker_end': function (e) {
         e.preventDefault();
-        $('.datetimepicker_begin').data("DateTimePicker").maxDate(e.date);
+        var date = moment(e.date).toDate();
+        Session.set("date_end", date);
+        // console.log("date_end: " + date);
     }
 
 });
