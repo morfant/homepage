d40c05cdb77638055b3952179a79698ff7c48a87
giy <giy.hands@gmail.com>
2018-04-15 13:47:01 +0900

    Add frontGate

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,5 +1,13 @@
 Template.layout.helpers({
     pageTitle: function () {
         return Session.get('pageTitle');
+    },
+    onGate: function() {
+        console.log(Router.current().route.getName());
+        if (Router.current().route.getName() == 'frontGate') {
+            return true;
+        } else {
+            return false;
+        }
     }
 });
---
+++ client/templates/includes/front_gate.js
@@ -0,0 +1,7 @@
+Template.frontGate.helpers({
+    convertImgLink: function(img_link_from_google_drive) {
+
+        console.log(convertGDlink(img_link_from_google_drive))
+        return convertGDlink(img_link_from_google_drive);
+    }
+});
