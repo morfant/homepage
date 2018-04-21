cd5b4032e4350bb50e50a68a41683592ab48e235
giy <giy.hands@gmail.com>
2018-04-21 19:30:50 +0900

    Add cv text editing function on frontSetting

--- client/templates/includes/cv.js
+++ client/templates/includes/cv.js
@@ -3,5 +3,18 @@ Template.cvPage.onCreated(function() {
 
 
 Template.cvPage.helpers({
+    getCVtext: function (){
+        var text = Settings.findOne({
+            cvText: {$exists: true}
+        });
+
+        if (!text) {
+            console.log("No cv text exists!");
+            return false;
+        }
+
+        lineBreakText = text.cvText.replace(/\n/g, "<br />");
+        return lineBreakText;
+    }
 
 });
--- client/templates/includes/frontSetting.js
+++ client/templates/includes/frontSetting.js
@@ -1,15 +1,15 @@
 Template.frontSetting.created = function () {
     console.log("frontSetting created()")
-    Session.set('frontSettingSubmitErrors', {});
+    Session.set('settingsSubmitErrors', {});
 }
 
 
 Template.frontSetting.helpers({
     errorMessage: function (field) {
-        return Session.get('frontSettingSubmitErrors')[field];
+        return Session.get('settingsSubmitErrors')[field];
     },
     errorClass: function (field) {
-        return !!Session.get('frontSettingSubmitErrors')[field] ? 'has-error' : '';
+        return !!Session.get('settingsSubmitErrors')[field] ? 'has-error' : '';
     },
     getFrontImageLink: function (){
         var link = Settings.findOne({
@@ -22,8 +22,21 @@ Template.frontSetting.helpers({
         }
 
         return link.frontImageLink;
+    },
+    getCVtext: function (){
+        var text = Settings.findOne({
+            cvText: {$exists: true}
+        });
 
+        if (!text) {
+            console.log("No cv text exists!");
+            return false;
+        }
+
+        return text.cvText;
     }
+
+
 
 });
 
@@ -35,12 +48,14 @@ Template.frontSetting.events({
 
         var settings = {
             frontImageLink: convertGDlink($(e.target).find('[name=front-image]').val()),
+            cvText: $(e.target).find('[name=cv-text]').val(),
         }
 
-        console.log(settings);
+        // console.log(settings);
 
         var errors = validateSettings(settings);
-        if (errors.frontImageLink) return Session.set('frontSettingSubmitErrors', errors);
+        if (errors.frontImageLink) return Session.set('settingsSubmitErrors', errors);
+        if (errors.cvText) return Session.set('settingsSubmitErrors', errors);
 
         Meteor.call('updateSetting', settings, function (error, result) { // display the error to the user and abort 
             if (error) return throwError(error.reason);
